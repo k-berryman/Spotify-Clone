9f5e4855c1e706809be91dbf3d11f1ce140aea14
Kaitlin Berryman <kaitlinberryman@gmail.com>
2022-02-24 20:08:44 -0500

    In the middle of adding playlists to sidebar. Messed up useSpotify hook / spotifyApi var

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -7,9 +7,21 @@ import {
   RssIcon,
 } from "@heroicons/react/outline";
 import { signOut, useSession } from "next-auth/react"
+import { useState, useEffect } from "react";
+import useSpotify from "../hooks/useSpotify"
 
 function Sidebar() {
+  const spotifyApi = useSpotify();
   const { data: session, status } = useSession();
+  const [playlists, setPlaylists] = useState([]);
+
+  // dependencies are session & spotifyApi
+  useEffect(() => {
+    // if the access token is set
+    console.log("woo")
+    }
+  , [session])
+
   console.log(session)
   return(
     <div className="text-gray-500 p-5 text-sm border-r border-gray-900 overflow-y-scroll scrollbar-hide h-screen ">
